1af3320a4fd805c5ca59cd2381308ff270b01064
Adam Hopkins <adamhopkins87@gmail.com>
2026-02-22 23:11:05 +0000

    chore: tidy schemas

--- studio/src/schemaTypes/index.ts
+++ studio/src/schemaTypes/index.ts
@@ -1,4 +1,4 @@
-import {aboutPage} from './documents/about-page'
-import {homePage} from './documents/home-page'
+import { aboutPage } from './documents/about-page';
+import { homePage } from './documents/home-page';
 
-export const schemaTypes = [homePage, aboutPage]
+export const schemaTypes = [homePage, aboutPage];
