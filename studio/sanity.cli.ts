29d3b6383fc92495de84b2271175981f7e00b48e
Adam Hopkins <adamhopkins87@gmail.com>
2026-02-22 23:11:27 +0000

    feat: setup cli for type gen

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -1,10 +1,10 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli';
 import path from 'path';
 
 export default defineCliConfig({
   api: {
-    projectId: 'o7fi7oqy',
-    dataset: 'production',
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+    dataset: process.env.SANITY_STUDIO_DATASET,
   },
   deployment: {
     /**
@@ -12,6 +12,17 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
+  },
+  schemaExtraction: {
+    path: '../sanity.schema.json',
+    enabled: true,
+    enforceRequiredFields: true,
+  },
+  typegen: {
+    path: './src/**/*.{ts,tsx,js,jsx}',
+    schema: '../sanity.schema.json',
+    generates: './sanity.types.ts',
+    overloadClientMethods: true,
   },
   vite: {
     resolve: {
