9f5a1ac839e3eb6ac13036becfa5a05ac7816d67
Adam Hopkins <adamhopkins87@gmail.com>
2026-02-24 22:29:23 +0000

    fix: warning with turbopack

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 
+import path from 'path';
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  /* config options here */
+  turbopack: {
+    root: path.join(import.meta.dirname, '..'),
+  },
 };
 
 export default nextConfig;
