89afaa5c97f598ad40e96fc7af92c3922b335820
Adam Hopkins <adamhopkins87@gmail.com>
2026-03-08 22:46:29 +0000

    refactor: layouts

--- frontend/src/components/_organisms/Navigation/SiteHeader/SiteHeader.tsx
+++ frontend/src/components/_organisms/Navigation/SiteHeader/SiteHeader.tsx
@@ -1,3 +1,4 @@
+import { SiteContainer } from '@atoms/SiteContainer';
 import { SiteLogo } from '@atoms/SiteLogo';
 import clsx from 'clsx';
 
@@ -6,15 +7,17 @@ import styles from './SiteHeader.module.css';
 export const SiteHeader = () => {
   return (
     <header className={clsx([styles.siteHeader])}>
-      <SiteLogo />
-      <nav className={clsx([styles.siteHeader__navContainer])}>
-        <ul>
-          <li>page 1</li>
-          <li>page 2</li>
-          <li>page 3</li>
-        </ul>
-      </nav>
-      <button>testsr</button>
+      <SiteContainer className={clsx([styles.siteHeader__container])}>
+        <SiteLogo />
+        <nav className={clsx([styles.siteHeader__navContainer])}>
+          <ul>
+            <li>page 1</li>
+            <li>page 2</li>
+            <li>page 3</li>
+          </ul>
+        </nav>
+        <button>testsr</button>
+      </SiteContainer>
     </header>
   );
 };
