2b58e75f82dae427729b3032bda290eac3696d55
Adam Hopkins <adamhopkins87@gmail.com>
2026-03-11 20:54:11 +0000

    feat: added button

--- frontend/src/components/_atoms/Button/Button.tsx
+++ frontend/src/components/_atoms/Button/Button.tsx
@@ -1,7 +1,23 @@
-import { type PropsWithChildren } from 'react';
+import clsx from 'clsx';
+import { type ComponentProps } from 'react';
 
-// interface IButtonProps {}
+import styles from './Button.module.css';
 
-export const Button = ({ children }: PropsWithChildren) => {
-  return <button>{children}</button>;
+interface IButtonProps extends ComponentProps<'button'> {
+  variant?: 'primary' | 'secondary';
+}
+
+export const Button = ({
+  children,
+  variant = 'primary',
+  ...rest
+}: IButtonProps) => {
+  return (
+    <button
+      className={clsx([styles.button, styles[`button__${variant}`]])}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 };
