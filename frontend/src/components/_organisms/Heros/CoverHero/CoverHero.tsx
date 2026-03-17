111b4e070ec7665d0ad9ba4245c1c07d4503c54d
adamhopkins <adamhopkins87@gmail.com>
2026-03-17 23:40:04 +0000

    refactor: hero

--- frontend/src/components/_organisms/Heros/CoverHero/CoverHero.tsx
+++ frontend/src/components/_organisms/Heros/CoverHero/CoverHero.tsx
@@ -1,11 +1,24 @@
+import clsx from 'clsx';
+
+import styles from './CoverHero.module.css';
+
 interface ICoverHeroProps {
   title: string;
+  subtitle: string;
+  label: string;
 }
 
-export const CoverHero = ({ title }: ICoverHeroProps) => {
+export const CoverHero = ({ title, label, subtitle }: ICoverHeroProps) => {
   return (
-    <section>
-      <h1>{title}</h1>
+    <section className={clsx([styles.coverHero])}>
+      <div className={styles.coverHero__container}>
+        <div className={styles.coverHero__contentWrapper}>
+          <p className={styles.coverHero__label}>{label}</p>
+          <h1>{title}</h1>
+          <p>{subtitle}</p>
+        </div>
+        <div className={styles.coverHero__imageWrapper}>image</div>
+      </div>
     </section>
   );
 };
