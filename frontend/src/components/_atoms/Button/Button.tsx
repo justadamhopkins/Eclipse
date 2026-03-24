efc02478e749ee4078c7b65a9e2135e993888623
Adam Hopkins <adamhopkins87@gmail.com>
2026-03-24 23:27:12 +0000

    feat: support link rendering in Button atom

--- frontend/src/components/_atoms/Button/Button.tsx
+++ frontend/src/components/_atoms/Button/Button.tsx
@@ -1,21 +1,47 @@
 import clsx from 'clsx';
+import Link from 'next/link';
 import { type ComponentProps } from 'react';
 
 import styles from './Button.module.css';
 
-interface IButtonProps extends ComponentProps<'button'> {
+type TButtonAsButton = {
+  href?: never;
   variant?: 'primary' | 'secondary';
-}
+} & ComponentProps<'button'>;
+
+type TButtonAsLink = {
+  href: string;
+  variant?: 'primary' | 'secondary';
+  children: React.ReactNode;
+  onClick?: React.MouseEventHandler<HTMLAnchorElement>;
+};
+
+type TButtonProps = TButtonAsButton | TButtonAsLink;
 
 export const Button = ({
   children,
   variant = 'primary',
   ...rest
-}: IButtonProps) => {
+}: TButtonProps) => {
+  const className = clsx([styles.button, styles[`button__${variant}`]]);
+
+  if ('href' in rest && rest.href) {
+    return (
+      <Link
+        href={rest.href}
+        onClick={(rest as TButtonAsLink).onClick}
+        className={className}
+      >
+        {children}
+      </Link>
+    );
+  }
+
   return (
     <button
-      className={clsx([styles.button, styles[`button__${variant}`]])}
-      {...rest}
+      type="button"
+      className={className}
+      {...(rest as Omit<TButtonAsButton, 'variant'>)}
     >
       {children}
     </button>
