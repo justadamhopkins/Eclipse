f82b8a1f5c2bb37bdfbbe68467b8358e04ee35aa
Adam Hopkins <adamhopkins87@gmail.com>
2026-03-24 00:10:12 +0000

    feat: wrapped gutter

--- frontend/src/components/_templates/HomePage/HomePage.tsx
+++ frontend/src/components/_templates/HomePage/HomePage.tsx
@@ -3,13 +3,15 @@ import { CoverHero } from '@organisms/Heros/CoverHero';
 
 export const HomePage = () => {
   return (
-    <Gutter>
+    <>
       <CoverHero
         label="Software engineer"
         title="Hi, i am Adam Hopkins"
         subtitle="Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth."
       />
-      <p>Home page content goes here.</p>
-    </Gutter>
+      <Gutter>
+        <p>Home page content goes here.</p>
+      </Gutter>
+    </>
   );
 };
