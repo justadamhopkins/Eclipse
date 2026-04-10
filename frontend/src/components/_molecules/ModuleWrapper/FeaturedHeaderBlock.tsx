8c851c22b962e4c6660a33c26ae33da84fc94f9a
Adam Hopkins <adamhopkins87@gmail.com>
2026-04-10 23:56:49 +0100

    feat: added module wrapper

---
+++ frontend/src/components/_molecules/ModuleWrapper/FeaturedHeaderBlock.tsx
@@ -0,0 +1,20 @@
+import clsx from 'clsx';
+import { type PropsWithChildren } from 'react';
+
+import styles from './FeaturedHeaderBlock.module.css';
+
+type TFeaturedHeaderBlockProps = {
+  title: string;
+};
+
+export const FeaturedHeaderBlock = ({
+  title,
+  children,
+}: PropsWithChildren<TFeaturedHeaderBlockProps>) => {
+  return (
+    <section className={clsx([styles.featuredHeaderBlock])}>
+      <h2>{title}</h2>
+      {children}
+    </section>
+  );
+};
