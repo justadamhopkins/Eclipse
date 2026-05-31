df3b0df8ec55385159092ca180b11cd65d9c72df
adamhopkins <adamhopkins87@gmail.com>
2026-05-31 23:34:13 +0100

    refactor: featured header block

--- frontend/src/components/_molecules/FeaturedHeaderBlock/FeaturedHeaderBlock.tsx
+++ frontend/src/components/_molecules/FeaturedHeaderBlock/FeaturedHeaderBlock.tsx
@@ -1,3 +1,4 @@
+import { Eyebrow } from '@atoms/Eyebrow';
 import clsx from 'clsx';
 import { type PropsWithChildren } from 'react';
 
@@ -5,14 +6,21 @@ import styles from './FeaturedHeaderBlock.module.css';
 
 type TFeaturedHeaderBlockProps = {
   title: string;
+  eyebrow: string;
 };
 
 export const FeaturedHeaderBlock = ({
   title,
+  eyebrow,
   children,
 }: PropsWithChildren<TFeaturedHeaderBlockProps>) => {
   return (
     <div className={clsx([styles.featuredHeaderBlock])}>
+      <Eyebrow
+        label={eyebrow}
+        variant="primary"
+        size="md"
+      />
       <h2>{title}</h2>
       {children}
     </div>
