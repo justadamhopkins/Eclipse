718b12552570a723227a325276c9e7a0fc4d14f3
Adam Hopkins <adamhopkins87@gmail.com>
2026-06-02 22:32:35 +0100

    refactor: fonts

--- frontend/src/styles/typography/fonts.ts
+++ frontend/src/styles/typography/fonts.ts
@@ -1,9 +1,25 @@
-import { Heebo } from 'next/font/google';
+import { Archivo, JetBrains_Mono, Playfair_Display } from 'next/font/google';
 
-export const FONT_HEEBO = Heebo({
+export const FONT_ARCHIVO = Archivo({
   display: 'swap',
   preload: true,
-  variable: '--font-heebo',
+  variable: '--font-archivo',
   subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700', '800', '900'],
+  weight: ['400', '500', '600', '700', '800', '900'],
+});
+
+export const FONT_JETBRAINS_MONO = JetBrains_Mono({
+  display: 'swap',
+  preload: false,
+  variable: '--font-jetbrains-mono',
+  subsets: ['latin'],
+  weight: ['400', '500'],
+});
+
+export const FONT_PLAYFAIR_DISPLAY = Playfair_Display({
+  display: 'swap',
+  preload: false,
+  variable: '--font-playfair-display',
+  subsets: ['latin'],
+  weight: ['700', '800'],
 });
