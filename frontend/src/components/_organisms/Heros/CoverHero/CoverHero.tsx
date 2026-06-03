be9ce096ee95be3c28228f16f25542b11f4a89af
Adam Hopkins <adamhopkins87@gmail.com>
2026-06-03 23:23:18 +0100

    feat: started cover hero refactor

--- frontend/src/components/_organisms/Heros/CoverHero/CoverHero.tsx
+++ frontend/src/components/_organisms/Heros/CoverHero/CoverHero.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@atoms/Button';
 import { Eyebrow } from '@atoms/Eyebrow';
-import clsx from 'clsx';
+import { ModuleSectionWrapper } from '@atoms/ModuleSectionWrapper';
 import NextImage from 'next/image';
 
 import styles from './CoverHero.module.css';
@@ -13,40 +13,47 @@ interface ICoverHeroProps {
 
 export const CoverHero = ({ title, label, subtitle }: ICoverHeroProps) => {
   return (
-    <section className={clsx([styles.coverHero])}>
-      <div className={styles.coverHero__contentWrapper}>
-        <div className={styles.coverHero__contentInner}>
-          <Eyebrow
-            variant="primary"
-            size="md"
-            label={label}
-          />
-          <h1>{title}</h1>
-          <p className={styles.coverHero__subtitle}>{subtitle}</p>
-          <div className={styles.coverHero__ctaRow}>
-            <Button
-              href="#footer"
+    <ModuleSectionWrapper>
+      <div className={styles.coverHero}>
+        <div className={styles.coverHero__contentWrapper}>
+          <div className={styles.coverHero__contentInner}>
+            <Eyebrow
               variant="primary"
-            >
-              View my work
-            </Button>
-            <Button
-              href="/contact"
-              variant="secondary"
-            >
-              Get in touch
-            </Button>
+              size="md"
+              label={label}
+            />
+            <h1>{title}</h1>
+            <p className={styles.coverHero__subtitle}>{subtitle}</p>
+            <div className={styles.coverHero__ctaRow}>
+              <Button
+                href="#footer"
+                variant="primary"
+              >
+                View my work
+              </Button>
+              <Button
+                href="/contact"
+                variant="secondary"
+              >
+                Get in touch
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.coverHero__imageContainer}>
+          <div className={styles.coverHero__imageGlass} />
+          <div className={styles.coverHero__imageWrapper}>
+            <div className={styles.coverHero__imageFrame}>
+              <NextImage
+                src="/adam.png"
+                alt="adam hopkins"
+                width={400}
+                height={400}
+              />
+            </div>
           </div>
         </div>
       </div>
-      <div className={styles.coverHero__imageWrapper}>
-        <NextImage
-          src="/adam.png"
-          alt="adam hopkins"
-          width={400}
-          height={400}
-        />
-      </div>
-    </section>
+    </ModuleSectionWrapper>
   );
 };
