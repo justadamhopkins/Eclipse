1607b579222d66e26f04349ecc06752837533a34
Adam Hopkins <adamhopkins87@gmail.com>
2026-06-29 23:48:22 +0100

    refactor(about-me): streamline AboutMe component by removing unnecessary markup

--- frontend/src/components/_molecules/FeaturedHeaderBlock/FeaturedHeaderBlock.tsx
+++ frontend/src/components/_molecules/FeaturedHeaderBlock/FeaturedHeaderBlock.tsx
@@ -6,11 +6,13 @@ import styles from './FeaturedHeaderBlock.module.css';
 type TFeaturedHeaderBlockProps = {
   title: string;
   eyebrow: string;
+  children?: React.ReactNode;
 };
 
 export const FeaturedHeaderBlock = ({
   title,
   eyebrow,
+  children,
 }: TFeaturedHeaderBlockProps) => {
   return (
     <div className={clsx([styles.featuredHeaderBlock])}>
@@ -22,33 +24,35 @@ export const FeaturedHeaderBlock = ({
         />
         <h2 className={styles.featuredHeaderBlock__heading}>{title}</h2>
       </header>
-      <div
-        className={clsx([
-          'u-flow--s-m',
-          styles.featuredHeaderBlock__bodyWrapper,
-        ])}
-      >
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
-          blanditiis, debitis explicabo impedit iusto perspiciatis placeat quo
-          voluptatem. Ab alias, eius enim minus molestias necessitatibus nulla
-          pariatur quibusdam rem sed.
-        </p>
+      {children || (
+        <div
+          className={clsx([
+            'u-flow--s-m',
+            styles.featuredHeaderBlock__bodyWrapper,
+          ])}
+        >
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
+            blanditiis, debitis explicabo impedit iusto perspiciatis placeat quo
+            voluptatem. Ab alias, eius enim minus molestias necessitatibus nulla
+            pariatur quibusdam rem sed.
+          </p>
 
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
-          blanditiis, debitis explicabo impedit iusto perspiciatis placeat quo
-          voluptatem. Ab alias, eius enim minus molestias necessitatibus nulla
-          pariatur quibusdam rem sed.
-        </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
+            blanditiis, debitis explicabo impedit iusto perspiciatis placeat quo
+            voluptatem. Ab alias, eius enim minus molestias necessitatibus nulla
+            pariatur quibusdam rem sed.
+          </p>
 
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
-          blanditiis, debitis explicabo impedit iusto perspiciatis placeat quo
-          voluptatem. Ab alias, eius enim minus molestias necessitatibus nulla
-          pariatur quibusdam rem sed.
-        </p>
-      </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
+            blanditiis, debitis explicabo impedit iusto perspiciatis placeat quo
+            voluptatem. Ab alias, eius enim minus molestias necessitatibus nulla
+            pariatur quibusdam rem sed.
+          </p>
+        </div>
+      )}
     </div>
   );
 };
