644562b7dafc0696d647b354b8539cefd1a0e8e1
adamhopkins <adamhopkins87@gmail.com>
2026-07-05 22:15:54 +0100

    feat: enhance About Me module layout and styles with responsive adjustments

--- frontend/src/components/_organisms/Modules/AboutMe/AboutMe.tsx
+++ frontend/src/components/_organisms/Modules/AboutMe/AboutMe.tsx
@@ -1,6 +1,9 @@
 import { ModuleSectionWrapper } from '@atoms/ModuleSectionWrapper';
+import { FactBox } from '@molecules/FactBox';
 import { FeaturedHeaderBlock } from '@molecules/FeaturedHeaderBlock';
+import clsx from 'clsx';
 
+import styles from './AboutMe.module.css';
 // export interface IAboutMeProps {
 //   description: string;
 // }
@@ -11,7 +14,33 @@ export const AboutMe = () => {
       <FeaturedHeaderBlock
         eyebrow="What i am about"
         title="About me"
-      />
+      >
+        <div className={styles.aboutMe__contentWrapper}>
+          <div className={clsx(['u-flow--s-m', styles.aboutMe__bodyWrapper])}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
+              blanditiis, debitis explicabo impedit iusto perspiciatis placeat
+              quo voluptatem. Ab alias, eius enim minus molestias necessitatibus
+              nulla pariatur quibusdam rem sed.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
+              blanditiis, debitis explicabo impedit iusto perspiciatis placeat
+              quo voluptatem. Ab alias, eius enim minus molestias necessitatibus
+              nulla pariatur quibusdam rem sed.
+            </p>
+          </div>
+          <FactBox
+            facts={[
+              { title: 'Based in', value: 'London' },
+              { title: 'Focus', value: 'London' },
+              { title: 'Availability', value: 'Open to roles' },
+              { title: 'Timezone', value: 'Gmt / Bst' },
+            ]}
+          />
+        </div>
+      </FeaturedHeaderBlock>
     </ModuleSectionWrapper>
   );
 };
