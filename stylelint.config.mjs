04edd6167e73829fc37cd0a84b9e7460c0291703
Adam Hopkins <adamhopkins87@gmail.com>
2026-08-18 00:30:22 +0100

    chore(stylelint): replace BEM pattern with camelCase class rule

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -3,9 +3,7 @@ export default {
   plugins: ['stylelint-selector-bem-pattern'],
   extends: ['stylelint-config-standard', 'stylelint-config-rational-order'],
   rules: {
-    'selector-class-pattern': null,
-    'plugin/selector-bem-pattern': {
-      preset: 'bem',
-    },
+    'selector-class-pattern': '^[a-z][a-zA-Z0-9]*$',
+    'selector-id-pattern': '^[a-z][a-zA-Z0-9]*$',
   },
 };
