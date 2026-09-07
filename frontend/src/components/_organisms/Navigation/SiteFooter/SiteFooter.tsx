ff7c4f857e4f398c280961824fe0f489ab78706b
Adam Hopkins <adamhopkins87@gmail.com>
2026-09-08 00:05:07 +0100

    feat: update SiteFooter with social links and branding
    - Add SiteLogo to footer
    - Add social media links using IconButton (GitHub, LinkedIn)
    - Add location info to footer
    - Restructure footer layout with proper sections

--- frontend/src/components/_organisms/Navigation/SiteFooter/SiteFooter.tsx
+++ frontend/src/components/_organisms/Navigation/SiteFooter/SiteFooter.tsx
@@ -1,5 +1,10 @@
+import { IconButton } from '@atoms/IconButton';
 import { SiteContainer } from '@atoms/SiteContainer';
+import { SiteLogo } from '@atoms/SiteLogo';
 import clsx from 'clsx';
+import NextLink from 'next/link';
+import { LuLinkedin } from 'react-icons/lu';
+import { RiGithubLine } from 'react-icons/ri';
 
 import styles from './SiteFooter.module.css';
 
@@ -10,7 +15,32 @@ export const SiteFooter = () => {
       className={clsx([styles.siteFooter])}
     >
       <SiteContainer>
-        <small>{new Date().getFullYear()}</small>
+        <SiteLogo />
+        <div>
+          <span>London, UK</span>
+          <ul>
+            <li>
+              {' '}
+              <IconButton
+                as={NextLink}
+                href="/contact"
+                variant="secondary"
+                icon={<RiGithubLine size={18} />}
+              />
+            </li>
+            <li>
+              <IconButton
+                as={NextLink}
+                href="/contact"
+                variant="secondary"
+                icon={<LuLinkedin size={18} />}
+              />
+            </li>
+          </ul>
+        </div>
+        <div>
+          <small>{new Date().getFullYear()}</small>
+        </div>
       </SiteContainer>
     </footer>
   );
