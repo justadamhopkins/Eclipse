0dc15da5ddbe03beb9599d11902c03838a13a19a
Adam Hopkins <adamhopkins87@gmail.com>
2026-09-08 00:05:02 +0100

    feat: implement ContactModule with variant theming
    - Build out ContactModule with FeaturedHeaderBlock and variant support
    - Add contact form with IconButton email link
    - Use ModuleSectionWrapper with secondary variant
    - Add ContactModule.module.css for layout

--- frontend/src/components/_organisms/Modules/ContactModule/ContactModule.tsx
+++ frontend/src/components/_organisms/Modules/ContactModule/ContactModule.tsx
@@ -1,7 +1,37 @@
+import { IconButton } from '@atoms/IconButton';
+import { ModuleSectionWrapper } from '@atoms/ModuleSectionWrapper';
+import { Text } from '@atoms/Text';
+import { FeaturedHeaderBlock } from '@molecules/FeaturedHeaderBlock';
+import NextLink from 'next/link';
+import { MdOutlineEmail } from 'react-icons/md';
+
+import styles from './ContactModule.module.css';
+
 export const ContactModule = () => {
   return (
-    <div>
-      <p>Contact module</p>
-    </div>
+    <ModuleSectionWrapper variant="secondary">
+      <FeaturedHeaderBlock
+        title="Contact"
+        variant="secondary"
+      >
+        <div className={styles.contactMeInnerWrapper}>
+          <Text
+            as="h3"
+            variant="headingXl"
+          >
+            Have a project in mind? Send me an email
+          </Text>
+          <IconButton
+            isLabelHiddenOnMobile={false}
+            variant="tertiary"
+            as={NextLink}
+            href="mailto:adamhopkins87@gmail.com"
+            icon={<MdOutlineEmail size={18} />}
+          >
+            Contact me
+          </IconButton>
+        </div>
+      </FeaturedHeaderBlock>
+    </ModuleSectionWrapper>
   );
 };
