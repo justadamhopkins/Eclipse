e270c4e2312626a5151ab9bfc0ec48ecf1cfb221
Adam Hopkins <adamhopkins87@gmail.com>
2026-09-15 00:24:32 +0100

    refactor: clean up SiteHeader and update utility types

--- frontend/src/types/utils.ts
+++ frontend/src/types/utils.ts
@@ -1,4 +1,5 @@
 export type TWithClassName<T> = T & { className?: string };
 export type TMaybeUndefined<T> = T | undefined;
+export type TMaybe<T> = T | null;
 export type TTshirtSize = 'sm' | 'md' | 'lg';
 export type TVariants = 'primary' | 'secondary' | 'tertiary' | 'brand';
